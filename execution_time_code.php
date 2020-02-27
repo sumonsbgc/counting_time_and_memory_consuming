46627614ceb612ff59d7d59faf503d611b002e10
Mohammad Sumon <sumonsbgc@gmail.com>
2020-02-27 18:22:06 +0600

    Update execution_time_code.php

--- execution_time_code.php
+++ execution_time_code.php
@@ -1,4 +1,17 @@
 <?php 
+$start = microtime(TRUE);
+
+$a = [];
+for ($i = 0; $i < 100000; $i++) {
+    $a[] = ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"];
+}
+
+$b = [];
+for ($i = 0; $i < 100000; $i++) {
+    $b[] = ["abc", "def", "ghi", "jkl", "mno", "pqr", "stu", "vwx", "yz"];
+}
+
+
 $end = microtime(TRUE);
 $execution_time = $end - $start;
 $execution_time_in_seconds = $execution_time / 1000000;
@@ -8,11 +21,13 @@ echo "The code took " . ( $execution_time_in_seconds ) . " seconds to complete."
 echo "<br>";
 echo "The code took " . ( $execution_time_in_minutes ) . " minutes to complete.";
 echo "<br>";
+
 function convert($size)
 {
     $unit = array('b', 'kb', 'mb', 'gb', 'tb', 'pb');
     return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . ' ' . $unit[$i];
 }
+
 echo convert(memory_get_usage(true));
 echo "<br>";
 echo convert(memory_get_peak_usage(true));
